cdea8937f9e94abd8b0b5d9e236105d77c899f6e
William Koller <williamkoller404@gmail.com>
2022-02-22 00:11:34 -0300

    test: should division 2 and 2 even if one of them is a string and the result must be 1

--- project-intro-jest/src/lib/calculator.spec.js
+++ project-intro-jest/src/lib/calculator.spec.js
@@ -33,6 +33,23 @@ describe('Calculator', () => {
     it('should division 2 and 2 and the result must be 1', () => {
       expect(division(2, 2)).toBe(1)
     });
+
+    it('should division 2 and 2 even if one of them is a string and the result must be 1', () => {
+      expect(division('2', '2')).toBe(1);
+
+      expect(() => {
+        sum([2, 2]);
+      }).toThrowError();
+
+      expect(() => {
+        sum({});
+      }).toThrowError();
+
+      expect(() => {
+        sum();
+      }).toThrowError();
+    });
+    
   });
   
 });
