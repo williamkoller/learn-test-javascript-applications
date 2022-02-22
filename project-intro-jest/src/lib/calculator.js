1fbda5bb7432568c9b74ee8a4c004a2966351dd5
William Koller <williamkoller404@gmail.com>
2022-02-22 00:11:16 -0300

    refactor: add parseInt in number and verify number nan

--- project-intro-jest/src/lib/calculator.js
+++ project-intro-jest/src/lib/calculator.js
@@ -9,5 +9,12 @@ module.exports.sum = (num1, num2) => {
 }
 
 module.exports.division = (num1, num2) => {
-  return num1 / num2;
+  const int1 = parseInt(num1);
+  const int2 = parseInt(num2);
+
+  if (Number.isNaN(int1) || Number.isNaN(int2)) {
+    throw new Error('Please check your input.');
+  }
+
+  return +int1 / +int2;
 }
