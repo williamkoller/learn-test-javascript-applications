67e53b347b1ca821b40c97fd136e940e1898d687
William Koller <williamkoller404@gmail.com>
2022-02-22 09:47:02 -0300

    refactor: add radix in parse int

--- project-intro-jest/src/lib/calculator.js
+++ project-intro-jest/src/lib/calculator.js
@@ -5,16 +5,16 @@ module.exports.sum = (num1, num2) => {
   if (Number.isNaN(int1) || Number.isNaN(int2)) {
     throw new Error('Please check your input.');
   }
-  return +int1 + +int2
+  return int1 + int2
 }
 
 module.exports.division = (num1, num2) => {
-  const int1 = parseInt(num1);
-  const int2 = parseInt(num2);
+  const int1 = parseInt(num1, 10);
+  const int2 = parseInt(num2, 10);
 
   if (Number.isNaN(int1) || Number.isNaN(int2)) {
     throw new Error('Please check your input.');
   }
 
-  return +int1 / +int2;
+  return int1 / int2;
 }
