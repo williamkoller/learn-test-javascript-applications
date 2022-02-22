c0cccba43dfc8698e980112301dc121ae6413235
William Koller <williamkoller404@gmail.com>
2022-02-22 00:05:16 -0300

    test: should division 2 and 2 and the result must be 1

--- project-intro-jest/src/lib/calculator.spec.js
+++ project-intro-jest/src/lib/calculator.spec.js
@@ -1,10 +1,10 @@
-const { sum } = require('./calculator');
+const { sum, division } = require('./calculator');
 
 describe('Calculator', () => {
   describe('sum()', () => {
     it('should sum 2 and 2 and the result must be 4', () => {
-    expect(sum(2, 2)).toBe(4);
-  });
+      expect(sum(2, 2)).toBe(4);
+    });
 
     it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
       expect(sum('2', '2')).toBe(4)
@@ -26,6 +26,12 @@ describe('Calculator', () => {
       expect(() => {
         sum();
       }).toThrowError();
+    });
+  });
+
+  describe('division()', () => {
+    it('should division 2 and 2 and the result must be 1', () => {
+      expect(division(2, 2)).toBe(1)
     });
   });
   
