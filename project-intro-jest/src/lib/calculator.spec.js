5b6dab8f04a837dcca099b082ccfe6830c89e618
will Koller <william.koller@invillia.com>
2022-02-24 22:34:44 -0300

    should subtraction 2 and 2 even if one of them is a string and the result must be 6

--- project-intro-jest/src/lib/calculator.spec.js
+++ project-intro-jest/src/lib/calculator.spec.js
@@ -90,6 +90,11 @@ describe('Calculator', () => {
     it('should multiplication 2 and 3 and the result must be 6', () => {
       expect(multiplication(2, 3)).toBe(6)
     });
+
+    it('should subtraction 2 and 2 even if one of them is a string and the result must be 6', () => {
+      console.log(multiplication('2', '3'))
+      expect(multiplication('2', '3')).toBe(6)
+    });
   });
   
 });
