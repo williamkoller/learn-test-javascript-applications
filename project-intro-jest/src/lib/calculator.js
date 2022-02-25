89faa714df39f09d34c973f7ade136e4f74fdbdc
will Koller <william.koller@invillia.com>
2022-02-24 22:34:57 -0300

    feat: add parse int in number

--- project-intro-jest/src/lib/calculator.js
+++ project-intro-jest/src/lib/calculator.js
@@ -31,5 +31,7 @@ module.exports.subtraction = (num1, num2) => {
 }
 
 module.exports.multiplication = (num1, num2) => {
-  return num1 * num2;
+  const int1 = parseInt(num1, 10);
+  const int2 = parseInt(num2, 10);
+  return int1 * int2;
 }
