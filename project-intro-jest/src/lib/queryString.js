28cd9504bf153eba629afdae2ea92f0e4a920418
William Koller <williamkoller404@gmail.com>
2022-03-07 22:39:05 -0300

    refactor: changing the code to cleaner

--- project-intro-jest/src/lib/queryString.js
+++ project-intro-jest/src/lib/queryString.js
@@ -1,4 +1,9 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check your params');
+  }
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
+  Object.entries(obj).map(keyValueToString).join('&');
